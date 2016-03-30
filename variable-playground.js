f063c363edf60c20ae878961a17c404a0141a3e3
Pavan Kale <pavankale1234@gmail.com>
2016-03-30 19:19:04 +0530

    Add variable playgroud area

---
+++ variable-playground.js
@@ -0,0 +1,27 @@
+/*var person = {
+	name: 'Pavan',
+	age: 21
+};
+
+function updatePerson (obj) {
+	obj = {
+		name: 'Pavan',
+		age: 27
+	};
+	obj.age = 24; 
+}
+
+updatePerson(person);
+console.log(person);*/
+
+//Array Example
+var grades = [15, 17];
+
+function addGrades(gradesArr) {
+	gradesArr.push(55);
+	debugger;
+	//gradesArr = [12,23,34];
+}
+
+addGrades(grades);
+console.log(grades);
