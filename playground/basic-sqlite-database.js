00c6bc51165feeabc650d46e66b6f179e926cdbf
Pavan Kale <pavankale1234@gmail.com>
2017-03-09 10:47:20 +0530

    Adding todo/user association

--- playground/basic-sqlite-database.js
+++ playground/basic-sqlite-database.js
@@ -19,19 +19,51 @@ var Todo = sequelize.define('todo', {
 	}
 });
 
+var User = sequelize.define('user', {
+	email: Sequelize.STRING
+});
+
+Todo.belongsTo(User);
+User.hasMany(Todo);
+
 sequelize.sync({
 	//force: true
 }).then(function () {
 	console.log('Everything is synched');
 
-	Todo.findById(2).then(function (todo) {
+	User.findById(1).then(function (user) {
+		user.getTodos({
+			where: {
+				completed: false
+			}
+		}).then(function (todos) {
+			todos.forEach(function (todo) {
+				console.log(todo.toJSON());
+			});
+		});
+	});
+
+	/*User.create({
+		email: 'pakale@bmc.com'
+	}).then(function() {
+		return Todo.create({
+			description: 'Clean yard'
+		});
+	}).then(function(todo) {
+		User.findById(1).then(function (user) {
+			user.addTodo(todo);
+		});
+	});*/
+
+
+	/*Todo.findById(2).then(function (todo) {
 		if(todo) {
 			console.log(todo.toJSON());
 		} else {
 			console.log('Todo not found');
 		}
 	});
-
+*/
 	/*Todo.create({
 		description: 'Take out trash',
 		//completed: false
@@ -61,6 +93,8 @@ sequelize.sync({
 		console.log(e);
 	});*/
 });
+
+
 
 //{force: true} => Drop table if already exist. By default true.
 //Id field is automatically created for each record. 
